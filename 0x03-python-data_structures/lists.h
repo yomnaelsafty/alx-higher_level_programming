d6c6d996f2837a59a9ef5463cb6514be9b604176
yomnaelsafty <yomna421@gmail.com>
2023-12-06 17:04:18 -0800

    p

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -24,5 +24,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-
+int aux_parlind(listint_t **head, listint_t *end);
 #endif /* LISTS_H */
